b1505e35ab3b3f0db71880ada7f00df0b9fedb16
Deebakkarthi Chinnasame Rani <dbk@deebakkarthi.com>
2026-07-21 08:18:51 -0400

    Add comparison

---
+++ scripts/artifact_join.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+join_rec() {
+	f1=$1
+	f2=$2
+	shift 2
+	if [ $# -gt 0 ]; then
+		join -t, "$f1" "$f2" | join_rec - "$@"
+	else
+		join -t, "$f1" "$f2"
+	fi
+}
+
+join_rec $(find artifacts/ -type f -name *.csv | xargs) | column -t

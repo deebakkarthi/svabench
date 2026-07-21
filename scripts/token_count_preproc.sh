d1e6e31f3699821029d2d0882cfee4d3215dd3fb
Deebakkarthi Chinnasame Rani <dbk@deebakkarthi.com>
2026-07-21 07:55:19 -0400

    change column for preproc

--- scripts/token_count_preproc.sh
+++ scripts/token_count_preproc.sh
@@ -4,7 +4,7 @@
 
 mapfile -t benchmark_arr < <(find preproc/ -type d -name rtl -printf "%h\n" | sort)
 
-echo "benchmark, tokens"
+echo "benchmark, tokens_after_preproc"
 for benchmark in "${benchmark_arr[@]}"; do
 	mapfile -t files_arr < <(find "$benchmark/rtl" -type f)
 	num_tokens="$(./scripts/_token_count.py "${files_arr[@]}")"

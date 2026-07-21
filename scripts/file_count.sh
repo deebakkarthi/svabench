708ba418b3e92f383f298ace1b441a8cd8278225
Deebakkarthi Chinnasame Rani <dbk@deebakkarthi.com>
2026-07-21 06:18:27 -0400

    file count

---
+++ scripts/file_count.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Prints "category/bench, num_files"
+
+mapfile -t benchmark_arr < <(find bench/ -type d -name rtl -printf "%h\n" | sort)
+
+for benchmark in "${benchmark_arr[@]}"; do
+	# Keep the files array around just in case
+	# I know that I can find the count by piping find into wc
+	# or using find itself. But the file names might be useful
+	# in the future
+	mapfile -t files_arr < <(find "$benchmark/rtl" -type f)
+	num_files="${#files_arr[@]}"
+	# Remove bench/ prefix
+	echo -e "${benchmark#*/}, $num_files"
+done
--- scripts/word_count.sh
+++ scripts/word_count.sh
@@ -2,10 +2,11 @@
 #
 # Return the input token count for all the benchmarks
 
-mapfile -t benchmark_arr < <(find bench/ -type d -name rtl -printf "%h\n")
+mapfile -t benchmark_arr < <(find bench/ -type d -name rtl -printf "%h\n" | sort)
 
 for benchmark in "${benchmark_arr[@]}"; do
 	mapfile -t files_arr < <(find "$benchmark/rtl" -type f)
 	num_tokens="$(cat "${files_arr[@]}" | wc -w | awk '{$1=$1};1')"
-	echo -e "$benchmark, $num_tokens"
+	# Remove bench/ prefix
+	echo -e "${benchmark#*/}, $num_tokens"
 done
